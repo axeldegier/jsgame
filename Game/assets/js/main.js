da3bf3c7df582bd489228400007ea99814c843d3
Axel de Gier <532600@student.glu.nl>
2020-10-24 23:37:32 +0200

    opdracht 5 en 6 af

--- Game/assets/js/main.js
+++ Game/assets/js/main.js
@@ -73,7 +73,8 @@ window.addEventListener(
 
        background = await Loader.loadImage('assets/img/backgrounds.png');
        sprites = await Loader.loadImage('assets/img/sprites.png');
-
+       
+       console.log(background);
        run();
 
     };
@@ -106,23 +107,58 @@ window.addEventListener(
         const speedPercent = speed / maxSpeed;
         const dx = dt * 2 * speedPercent;
 
+        startPosition = position;
+
         position = Util.increase(position, dt * speed, trackLength);
 
+        skyOffset = Util.increase(skyOffset, (skySpeed * playerSegment.curve * (position - startPosition)) / segmentLength, 1);
+        hillsOffset = Util.increase(hillsOffset, (hillsSpeed * playerSegment.curve * (position - startPosition)) / segmentLength, 1);
+        woodsOffset = Util.increase(woodsOffset, (woodsSpeed * playerSegment.curve * (position - startPosition)) / segmentLength, 1);
+
         if(keyLeft){
+           if(speed > 0){
             playerX -= dx;
-            
-        }else if(keyRight) {
-            playerX += dx;
+            if(hangTimer >= hangDelay) {
+                hangTimer = 0;
+                hang -= 2;
+                hang = Util.limit(hang, -6, 0);
+            }else{
+                hangTimer += dt * 1000;
+            }
         }
+        }else if(keyRight) {
+            if(speed > 0){
+                playerX = playerX + dx;
+                if(hangTimer >= hangDelay) {
+                    hangTimer = 0;
+                    hang += 2;
+                    hang = Util.limit(hang, 0, 6);
+                }else{
+                    hangTimer += dt * 1000;
+                }
+        }
+    }else{
+        if(hang !== 0) {
+            if(hangTimer >= hangDelay || hangTimer === 0) {
+                hangTimer = 0;
+                hang = hang < 0 ? hang + 2 : hang - 2;
+
+            }
+            hangTimer += dt * 1000;
+        }
+    }
 
         playerX = playerX - dx * speedPercent * playerSegment.curve * centrifugal
 
-        if(keyFaster) {
-            speed = Util.accelerate(speed, accel, dt);
-        }else if (keySlower) {
+        if(keySlower) {
             speed = Util.accelerate(speed, breaking, dt);
+            brake = 14;
+        }else if (keyFaster) {
+            speed = Util.accelerate(speed, accel, dt);
+            brake = 0;
         }else{
             speed = Util.accelerate(speed, decel, dt);
+            brake = 0;
         }
 
         if((playerX < -1 || playerX > 1) && speed > offRoadLimit) {
@@ -131,6 +167,9 @@ window.addEventListener(
 
         playerX = Util.limit(playerX, -2, 2);
         speed = Util.limit(speed, 0, maxSpeed);
+        tire = Util.toInt(position / 500) % 2;
+
+        bikeSpriteSelector = 6 + tire + hang + brake;
     };
 
    const render = () => {
@@ -145,6 +184,10 @@ window.addEventListener(
     let dx = -(baseSegment.curve * basePercent);
     
     ctx.clearRect(0, 0, width, height);
+
+    Render.background(ctx, background, width, height, BACKGROUND.SKY, skyOffset, resolution * skySpeed * playerY);
+    Render.background(ctx, background, width, height, BACKGROUND.HILLS, hillsOffset, resolution * hillsSpeed * playerY);
+    Render.background(ctx, background, width, height, BACKGROUND.WOODS, woodsOffset, resolution * woodsSpeed * playerY);
     
     let n, i, segment, car, sprite, spriteScale, spriteX, spriteY;
     
@@ -170,5 +213,14 @@ window.addEventListener(
      Render.segment(ctx, width, lanes, segment.p1.screen.x, segment.p1.screen.y, segment.p1.screen.w, segment.p2.screen.x, segment.p2.screen.y, segment.p2.screen.w, segment.fog, segment.color);
      maxy = segment.p1.screen.y;
     }
+
+    for(n=drawDistance-1; n>0; n--) {
+        segment = segments[(baseSegment.index + n) % segments.length];
+        if (segment == playerSegment) {
+            Render.player(ctx, width, height, resolution, roadWidth, sprites, speed / maxSpeed, cameraDepth / playerZ, width / 2, height / 2 - ((cameraDepth / playerZ) * Util.interpolate(playerSegment.p1.camera.y, playerSegment.p2.camera.y, playerPercent) * height) / 2); // speed * (keyLeft ? -1 : keyRight ? 1 : 0), playerSegment.p2.world.y - playerSegment.p1.world.y
+          }
+
+    }
+
 
    };
